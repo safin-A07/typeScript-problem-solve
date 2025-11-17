271a19f5e7eab2ff676f1af4a9bac2ab66a86009
safin_A07 <safinhossain@9339@gmail.com>
2025-11-17 23:50:04 +0600

    solution done

---
+++ solution.ts
@@ -0,0 +1,139 @@
+function formatValue (value : string | number | boolean) {
+    if(typeof value == 'string'){
+        return value.toUpperCase();
+    }
+    else if(typeof value === 'number'){
+       return value * 10 ;
+    }
+    else if(typeof value === 'boolean'){
+        if(value === false){
+            return true;
+        }
+        else{
+            return false ;
+        }
+    }
+}
+
+function getLength(value : string | any[]){
+    if(typeof value === "string"){
+        let number =value.length;
+        return number ;
+    }
+    else if(Array.isArray(value)){
+        let arraySize = value.length;
+        return arraySize ;
+    }
+
+}
+
+class Person {
+    name : string ;
+    age : number ;
+    constructor (name : string , age : number){
+        this.name = name ;
+        this.age = age ;
+    }
+    getDetails(){
+        return `'Name: ${this.name}, Age: ${this.age}'`
+    }
+
+}
+
+function filterByRating(values : {title : string , rating:number}[]){
+    let arr : {title : string , rating:number}[]=[];
+    values.forEach(book =>{
+        if(book.rating >= 4)
+        {
+            arr.push(book);
+        }
+    });
+    return arr ;
+}
+
+function filterActiveUsers(value : { id: number, name: string, email: string, isActive: boolean }[]){
+    let arr : { id: number, name: string, email: string, isActive: boolean }[]=[];
+    value.forEach(val =>{
+        if(val.isActive === true){
+            arr.push(val)
+        }
+    })
+    return arr ;
+}
+
+interface Book {
+    title: string,
+    author: string,
+    publishedYear: number,
+    isAvailable: boolean
+}
+function printBookDetails (value :Book){
+    return console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: ${value.isAvailable?"Yes" :"No"}`)
+}
+
+
+function getUniqueValues (arr1: number[], arr2: number[]): number[] {
+  const result: number[] = [];
+
+ 
+  for (let i = 0; i < arr1.length; i++) {
+    let exists = false;
+
+   
+    for (let j = 0; j < result.length; j++) {
+      if (arr1[i] === result[j]) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      result[result.length] = arr1[i];
+    }
+  }
+
+
+  for (let i = 0; i < arr2.length; i++) {
+    let exists = false;
+
+    for (let j = 0; j < result.length; j++) {
+      if (arr2[i] === result[j]) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      result[result.length] = arr2[i];
+    }
+  }
+
+  return result;
+}
+
+
+type Product = {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number; 
+}
+
+function calculateTotalPrice(products: Product[]): number {
+    let total = 0;
+
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+
+        const itemTotal = product.price * product.quantity;
+
+      
+        const discountAmount = product.discount ? (itemTotal * product.discount) / 100 : 0;
+
+        const totalItemPrice = itemTotal - discountAmount;
+
+        total += totalItemPrice;
+    }
+
+    return total;
+}
+
+
